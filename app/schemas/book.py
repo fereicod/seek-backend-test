48000d8bbe69ae2dc335a3f0fb9f721cddd74d58
Fernando <mferna.92@gmail.com>
2026-01-31 17:51:00 -0600

    feat(book): new schemas for book

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -2,6 +2,13 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
+class BookRequest(BaseModel):
+    title: str
+    author: str
+    published_date: datetime
+    genre: str
+    price: float
+
 class BookResponse(BaseModel):
     id: str
     title: str
@@ -9,3 +16,11 @@ class BookResponse(BaseModel):
     published_date: datetime
     genre: str
     price: float
+
+
+class SuccessResponse(BaseModel):
+    success: bool
+
+
+class BookUpdateResponse(SuccessResponse):
+    book: BookResponse | None = None
