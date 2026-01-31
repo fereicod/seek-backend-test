05450be52811cd34322a44a2a4480d568aeed89f
Fernando <mferna.92@gmail.com>
2026-01-30 22:06:00 -0600

    feat(db): add mongo db connection and collection

---
+++ app/db/mongo.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+from app.core.config import settings
+
+# MongoDB Client Setup
+client = MongoClient(settings.MONGO_URI)
+db = client[settings.MONGO_DB_NAME]
+
+# Collections
+users_collection = db["users"]
+books_collection = db["books"]
+refresh_tokens_collection = db["refresh_tokens"]
