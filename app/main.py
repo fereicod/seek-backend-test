0dde3a892cab660fac1ee0d2860c5a3842e3f8c6
Fernando <mferna.92@gmail.com>
2026-01-31 11:34:00 -0600

    feat(main): add router to start project

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from seek-backend-test!")
+from fastapi import FastAPI
+from app.routers import auth, books
 
+app = FastAPI(title="Books API", version="1.0.0")
 
-if __name__ == "__main__":
-    main()
+prefix = "/api/v1"
+app.include_router(auth.router, prefix=prefix, tags=["Authentication"])
+app.include_router(books.router, prefix=prefix, tags=["Books"])
